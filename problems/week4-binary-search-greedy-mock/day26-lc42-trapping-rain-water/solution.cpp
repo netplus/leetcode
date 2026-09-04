2b7afa8d37dcf65427030a5026f6d71dedc47842
copy <netplusplus@outlook.com>
2026-09-05 02:33:50 +0800

    metadata: sync lc42 official examples

--- problems/week4-binary-search-greedy-mock/day26-lc42-trapping-rain-water/solution.cpp
+++ problems/week4-binary-search-greedy-mock/day26-lc42-trapping-rain-water/solution.cpp
@@ -7,6 +7,16 @@
 // 题目描述：
 // 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
 //
+// 题目示例：
+// 示例 1：
+//   Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+//   Output: 6
+//   Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+//
+// 示例 2：
+//   Input: height = [4,2,0,3,2,5]
+//   Output: 9
+//
 // 约束与要求：
 //   - n == height.length
 //   - 1 <= n <= 2 * 10^4

8492295ecfb68b349e985f24cd86a6c7b7a6a769
copy <netplusplus@outlook.com>
2026-09-05 09:55:51 +0800

    docs: register Week2 Day10 code comments

--- tools/refined_data.py
+++ tools/refined_data.py
@@ -46,6 +46,7 @@ from code_comments_week1_day6 import CODE_COMMENTS_WEEK1_DAY6
 from code_comments_week1_day7 import CODE_COMMENTS_WEEK1_DAY7
 from code_comments_week2_day8 import CODE_COMMENTS_WEEK2_DAY8
 from code_comments_week2_day9 import CODE_COMMENTS_WEEK2_DAY9
+from code_comments_week2_day10 import CODE_COMMENTS_WEEK2_DAY10
 from chinese_titles import validate_title_coverage
 
 
@@ -139,6 +140,7 @@ _CODE_COMMENT_MODULES = (
     CODE_COMMENTS_WEEK1_DAY7,
     CODE_COMMENTS_WEEK2_DAY8,
     CODE_COMMENTS_WEEK2_DAY9,
+    CODE_COMMENTS_WEEK2_DAY10,
 )
 
 CODE_COMMENTS = {}

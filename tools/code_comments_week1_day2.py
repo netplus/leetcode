477e7d033e213c22c18320dff6454706a2beb7f2
copy <netplusplus@outlook.com>
2026-09-05 09:06:14 +0800

    docs: explain LC-15 key three-sum code

--- tools/code_comments_week1_day2.py
+++ tools/code_comments_week1_day2.py
@@ -38,4 +38,55 @@ public:
         return {};
     }
 };''',
+
+    15: r'''// ---------- Solution ----------
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // 排序不是为了方便输出，而是为了两件事：
+        // 1) 固定锚点后，剩余两数具有单调性，可以复用 LC-167 双指针；
+        // 2) 相同值会相邻，便于按“值”去掉重复三元组。
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> answer;
+        const int n = static_cast<int>(nums.size());
+
+        for (int i = 0; i < n - 2; ++i) {
+            // 相同锚点值会产生完全相同的后续搜索空间。
+            // 若不在开局前跳过，像 [-1,...] 的同一个值会重复生成同样的三元组。
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            // 排序后 nums[i] 已是当前及后续三元组中的最小值。
+            // 一旦 nums[i] > 0，后面的两个数只会 >= nums[i] > 0，
+            // 三数之和不可能再回到 0，因此可以结束整个外层循环，而不只是 continue。
+            if (nums[i] > 0) break;
+
+            // 固定 nums[i] 后，问题降维成：
+            // 在有序区间 [i+1, n-1] 中寻找两数和 == -nums[i]。
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                const long long sum = static_cast<long long>(nums[i]) + nums[left] + nums[right];
+                if (sum < 0) {
+                    // 当前 right 已是最大候选；连它都使总和偏小，
+                    // 当前 left 与任何更小/相等的右端配对都不可能成功，所以排除 left。
+                    ++left;
+                } else if (sum > 0) {
+                    // 对称地，当前 left 已是最小候选；连它都使总和偏大，
+                    // 当前 right 与任何更大/相等的左端配对都不可能成功，所以排除 right。
+                    --right;
+                } else {
+                    // 此时三个“值”已经确定为一个合法答案。
+                    answer.push_back({nums[i], nums[left], nums[right]});
+
+                    // 题目要求的是不重复的值三元组，而不是所有不同下标组合。
+                    // 记住本次命中的左右值，然后一次跨过所有相同值；
+                    // 否则同一个锚点会反复输出完全相同的三元组。
+                    const int lv = nums[left], rv = nums[right];
+                    while (left < right && nums[left] == lv) ++left;
+                    while (left < right && nums[right] == rv) --right;
+                }
+            }
+        }
+        return answer;
+    }
+};''',
 }
